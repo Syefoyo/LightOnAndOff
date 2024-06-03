b591ee8ad45b8a8f4dfe68232bce07e6f66e6bea
Syefoyo <trisyefolionel@gmail.com>
2024-06-03 18:34:21 +0700

    onAndoff

---
+++ script.js
@@ -0,0 +1,16 @@
+function darkMode() {
+    const body = document.getElementById("bgc");
+    body.style.backgroundColor = "grey";
+}
+function lightMode() {
+    const bodye = document.getElementById("bgc");
+    bodye.style.backgroundColor = "white";
+}
+function lightOff() {
+    const images = document.getElementById("off");
+    images.src = "bulboff.png";
+}
+function lightOn() {
+const imagess = document.getElementById("off");
+imagess.src = "bulbon.jpg";
+}
